db01f1ac7b8754e3e50fccdad809cc631e5de5da
Daffy <62877907+Seikanji@users.noreply.github.com>
2024-12-03 21:10:28 +0100

    Level one
    completed

---
+++ 2024/Level_1/Level1_2.py
@@ -0,0 +1,28 @@
+a = []
+b = []
+
+file = open("input.txt", "r")
+for x in file:
+    number1 = ""
+    number2 = ""
+    index = 0;
+    for i in x:
+        if(index < 5):
+            number1 += i
+        elif(index> 7):
+            number2 += i
+        index += 1
+    a.append(int(number1))
+    b.append(int(number2))
+
+        
+file.close()
+
+a.sort()
+b.sort()
+
+result = 0
+for i in range(0,len(a)):
+    result += (a[i]*b.count(a[i]))
+
+print(result)
